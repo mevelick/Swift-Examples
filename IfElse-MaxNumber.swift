1f5409c12ff67368e752db75789b6006210553c6
mevelick <77936515+mevelick@users.noreply.github.com>
2021-10-27 11:46:51 -0400

    If/Else - Max Number
    A very basic if/else statement that prints the maximum of 2 constants.

---
+++ IfElse-MaxNumber.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+// Declare 2 constants. Print the largest of the two.
+
+let x = 89
+let y = 7
+
+if x > y {
+	print(x)
+}
+
+else if y > x {
+	print(y)
+}
+else {
+	print("x = y")
+}
