882ce8829cdcdeb77a71be419cff5723f8c4cc98
mevelick <77936515+mevelick@users.noreply.github.com>
2021-10-26 22:20:24 -0400

    Family Birth Year Function
    Demonstrates the use of a simple print function that accepts 2 parameters.

---
+++ FamilyBirthdayFunction.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func printFamilyBirthdays(name: String, year: Int){
+	print("\(name) was born in the year \(year).")
+}
+
+printFamilyBirthdays(name: "Mom", year: 1954)
+printFamilyBirthdays(name: "Dad", year: 1954)
+printFamilyBirthdays(name: "Bubbie", year: 1926)
+printFamilyBirthdays(name: "Me", year: 1989)
+printFamilyBirthdays(name: "Karsen", year: 1991)
+printFamilyBirthdays(name: "Josh", year: 1992)
