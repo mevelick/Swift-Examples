0592f453ecf5138f9654440b364ac1ecd94d075b
mevelick <77936515+mevelick@users.noreply.github.com>
2021-10-26 21:54:22 -0400

    Swap
    Swapping two variables

---
+++ swap.swift
@@ -0,0 +1,11 @@
+var green = "banana"
+var yellow = "kiwi"
+
+//switch the variables so that they are correct
+
+var temp = green
+green = yellow
+yellow = temp
+
+print("The color of a \(green) is green.")
+print("The color of a \(yellow) is yellow.")
