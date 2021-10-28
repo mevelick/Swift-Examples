0ea561e3a9fba373c7bfc2483c23b9bbfa3a82fc
Marni <mevelick@gmail.com>
2021-10-28 17:56:02 -0400

    Boolean and Logical Operator

---
+++ boolAndLogicalOperator.swift
@@ -0,0 +1,11 @@
+// Create a program using a logical operator and Boolean variables.
+
+var working = true
+var studying = false
+
+if working || studying {
+  print("Turn off phone and TV.")
+}
+else {
+  print("It is okay to be on phone or watch TV.")
+}
