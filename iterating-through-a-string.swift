620c1df8085664e80b04cb94443be5ecfcb740be
Marni <mevelick@gmail.com>
2021-11-02 21:46:10 -0400

    Iterating through a quote

---
+++ iterating-through-a-string.swift
@@ -0,0 +1,38 @@
+var quote = "Don't just exist. Live."
+
+for char in quote {
+  if char == "t" {
+      print("It's a 't'!")
+  }
+  else{
+    print(char)
+  }
+}
+
+/* Output:
+
+D
+o
+n
+'
+It's a 't'!
+ 
+j
+u
+s
+It's a 't'!
+ 
+e
+x
+i
+s
+It's a 't'!
+.
+ 
+L
+i
+v
+e
+.
+
+*/
