f5a1e7ce1b761c0faaaf622c155eb3ac6c5d96bd
Marni <mevelick@gmail.com>
2021-11-06 08:27:05 -0400

    FizzBuzz Problem

---
+++ FizzBuzz.swift
@@ -0,0 +1,16 @@
+//My solution to the 'FizzBuzz' problem:
+
+for i in 1...100 {
+  if (i%3==0 && i%5==0){
+    print("FizzBuzz")
+  }
+  else if (i%3==0){
+    print("Fizz")
+  }
+  else if (i%5==0){
+    print("Buzz")
+  }
+  else{
+  print(i)
+  }
+}
